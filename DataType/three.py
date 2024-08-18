72ae2501a36b55ce9879bfa6c64a43ff7a55a454
muktiswarup <csjena1999@gmail.com>
2024-08-18 12:55:45 +0530

    'string'

--- DataType/three.py
+++ DataType/three.py
@@ -1,6 +1,10 @@
-l1=[23,25,45]
-b=bytes(l1)
-print(b)
+# l1=[23,25,45]
+# b=bytes(l1)
+# print(b)
 
-ba=bytearray(l1)
-print(ba)
+# ba=bytearray(l1)
+# print(ba)
+
+ra=range(4,14,2)
+for i in ra:
+    print(i) 
